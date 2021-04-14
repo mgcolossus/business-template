28ab62461e89d65b25e738a054dc2b9e561975d6
Magomed <colossus130600@yandex.ru>
2021-04-14 20:19:37 +0300

    Add 'news-and-media' section

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-const headerMenu = document.querySelector('.header__menu');
-const headerNav = document.querySelector('.header__nav');
+const headerMenu = document.querySelector(".header__menu");
+const headerNav = document.querySelector(".header__nav");
 const body = document.body;
 
-console.log(headerMenu, headerNav)
+headerMenu.addEventListener("click", function () {
+  headerMenu.classList.toggle("active");
+  headerNav.classList.toggle("active");
+  body.classList.toggle("lock");
+});
 
-headerMenu.addEventListener('click', function () {
-  headerMenu.classList.toggle('active');
-  headerNav.classList.toggle('active');
-  body.classList.toggle('lock');
-})
+function showSelectedCategoryItems(type) {
+  if (type === "all") {
+    return newsItems.forEach((item) => {
+      item.style.display = "";
+    });
+  }
+  newsItems.forEach(item => {
+    if (item.dataset.category === type) {
+      item.style.display = "";
+    } else {
+      item.style.display = 'none';
+    }
+  })
+}
+
+
+const tabSelectorClass = ".news-and-media__tabs-item";
+const tabActiveSelectorClass = "news-and-media__tabs-item_active";
+
+const tabsSelectors = [...document.querySelectorAll(tabSelectorClass)];
+const newsItems = [...document.querySelectorAll(".news-item")];
+
+for (tabSelector of tabsSelectors) {
+  tabSelector.addEventListener("click", function (e) {
+    tabsSelectors.forEach((tab) => {
+      if (tab.classList.contains(tabActiveSelectorClass)) {
+        tab.classList.remove(tabActiveSelectorClass);
+      }
+      e.target.classList.add(tabActiveSelectorClass);
+      showSelectedCategoryItems(e.target.dataset.category);
+    });
+  });
+}
+
+
